78ced83dbefcc2e4a075c7f3f4415d461443b68f
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-10-15 16:00:33 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Layout } from './Layout';
+import { GlobalStyle } from './GlobalStyle';
+import { FeedbackOptions } from './FeedbackSet/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Layout>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            handleFeedback={this.handleFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() <= 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              percentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: raw;
+  gap: 60px;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 15px;
+`;
